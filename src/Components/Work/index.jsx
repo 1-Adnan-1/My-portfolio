9e9743b25a1cf49b14c0272f426bbfa9040031c4
1-adnan-1 <altintasadnan9@gmail.com>
2025-04-10 00:08:32 +0300

    work update

--- src/Components/Work/index.jsx
+++ src/Components/Work/index.jsx
@@ -5,6 +5,16 @@ import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
 import Wideo1 from '../../assets/images/p-w.mp4'
 import Wideo2 from '../../assets/images/p-w2.mp4'
+import p1 from '../../assets/p-image/p-1.png'
+import p2 from '../../assets/p-image/p-2.png'
+import p3 from '../../assets/p-image/p-3.png'
+import p4 from '../../assets/p-image/p-4.png'
+import p5 from '../../assets/p-image/p-5.png'
+import p6 from '../../assets/p-image/p-6.png'
+import p7 from '../../assets/p-image/p-7.png'
+import p8 from '../../assets/p-image/p-8.png'
+import p9 from '../../assets/p-image/p-9.png'
+import p10 from '../../assets/p-image/p-10.png'
 
 const Work = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -16,61 +26,61 @@ const Work = () => {
     {
       id: 1,
       title: 'E Commerce ',
-      image: '../../assets/p-image/p-1.png',
+      image: p1,
       link: 'https://github.com/1-Adnan-1/Toolkit-Ecommerce',
     },
     {
       id: 2,
       title: 'Cyrpto Place',
-      image: '../../assets/p-image/p-2.png',
+      image: p2,
       link: 'https://github.com/1-Adnan-1/Cyrpto-place',
     },
     {
       id: 3,
       title: 'Youtube Clone',
-      image: '../../assets/p-image/p-3.png',
+      image: p3,
       link: 'https://github.com/adnanaltintas/youtube-clone',
     },
     {
       id: 4,
       title: 'X Clone',
-      image: '../../assets/p-image/p-4.png',
+      image: p4,
       link: 'https://github.com/1-Adnan-1/Twitter-clone',
     },
     {
       id: 5,
       title: 'Car Rental',
-      image: '../../assets/p-image/p-5.png',
+      image: p5,
       link: 'https://github.com/1-Adnan-1/Car-Rental',
     },
     {
       id: 6,
       title: 'Flight Radar',
-      image: '../../assets/p-image/p-6.png',
+      image: p6,
       link: 'https://github.com/1-Adnan-1/Flight-Radar',
     },
     {
       id: 7,
       title: 'Netflix Clone',
-      image: '../../assets/p-image/p-7.png',
+      image: p7,
       link: 'https://github.com/1-Adnan-1/ReactRedux-Netflix',
     },
     {
       id: 8,
       title: 'Hotels App',
-      image: '../../assets/p-image/p-8.png',
+      image: p8,
       link: 'https://github.com/1-Adnan-1/Hotels-App',
     },
     {
       id: 9,
       title: 'Translate App',
-      image: '../../assets/p-image/p-9.png',
+      image: p9,
       link: 'https://github.com/1-Adnan-1/Translate-App',
     },
     {
       id: 10,
       title: 'Nots App',
-      image: '../../assets/p-image/p-10.png',
+      image: p10,
       link: 'https://github.com/1-Adnan-1/Notlar',
     },
   ]
