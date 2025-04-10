f38d887083813f8a9917109a3dafe03cf99600fd
1-adnan-1 <altintasadnan9@gmail.com>
2025-04-10 12:14:57 +0300

    responsive

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -8,10 +8,14 @@ import {
   faEnvelope,
   faHome,
   faUser,
-  faX,
+  faXmark,
 } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn'
-import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import {
+  faLinkedinIn,
+  faGithub,
+  faInstagram,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -23,14 +27,20 @@ const Sidebar = () => {
   return (
     <>
       <div className="toggle-icon" onClick={toggleSidebar}>
-        ☰
+        <FontAwesomeIcon icon={isOpen ? faXmark : faBars} color="#ffd700" />
       </div>
+
       <div className={`nav-bar ${isOpen ? 'open' : ''}`}>
         <div className="logo">
           <img src={logoSubtitle} alt="logo" />
         </div>
         <nav>
-          <NavLink exact="true" activeclassname="active" to="/">
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/"
+            onClick={toggleSidebar}
+          >
             <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
           </NavLink>
           <NavLink
@@ -38,6 +48,7 @@ const Sidebar = () => {
             activeclassname="active"
             className="about-link"
             to="/about"
+            onClick={toggleSidebar}
           >
             <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
           </NavLink>
@@ -46,6 +57,7 @@ const Sidebar = () => {
             activeclassname="active"
             className="work-link"
             to="/work"
+            onClick={toggleSidebar}
           >
             <FontAwesomeIcon icon={faBars} color="#4d4d4e" />
           </NavLink>
@@ -54,6 +66,7 @@ const Sidebar = () => {
             activeclassname="active"
             className="contact-link"
             to="/contact"
+            onClick={toggleSidebar}
           >
             <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
           </NavLink>
@@ -76,7 +89,7 @@ const Sidebar = () => {
             >
               <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
             </a>
-          </li>{' '}
+          </li>
           <li>
             <a
               target="_blank"
@@ -85,14 +98,14 @@ const Sidebar = () => {
             >
               <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
             </a>
-          </li>{' '}
+          </li>
           <li>
             <a
               target="_blank"
               rel="noreferrer"
               href="https://x.com/1ADnanSAmi1"
             >
-              <FontAwesomeIcon icon={faX} color="#4d4d4e" />
+              <FontAwesomeIcon icon={faXTwitter} color="#4d4d4e" />
             </a>
           </li>
         </ul>
